bf6f11c4038d19b275b0d4f3fe9b0bc2e69e320d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-06-23 22:06:45 +0530

    added function for mouse follow

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menuButton = document.querySelector("#menu");
 const navPart2 = document.querySelector(".nav-part2");
+const circle = document.querySelector("#mini-circle");
 
 menuButton.addEventListener("click", () => {
   menuButton.style.display = "none";
@@ -11,3 +12,12 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 });
+
+function circleMouseFollow() {
+  window.addEventListener("mousemove", (details) => {
+    circle.style.transform = `
+    translate(${details.clientX}px, ${details.clientY}px)
+    `;
+  });
+}
+circleMouseFollow();
